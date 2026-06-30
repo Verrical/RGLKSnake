ca0676d6254fd1e402585f821d32958105cc2355
Verrical <16.vertix.es@gmail.com>
2026-06-30 19:46:13 +0700

    Prototype v0.01
    -updated ability: now breaks obstacles and gives score on successful rocks destroyed
    -added return to menu button
    -theres an actual scoring system now

---
+++ Scripts/FoodSpawner.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour
+{
+    public static FoodSpawner Instance;
+
+    private Vector2Int foodGridPos;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        SpawnFood(null);
+    }
+
+    public void CheckEat(Vector2Int headPos, SnakeController snake)
+    {
+        if (headPos == foodGridPos)
+        {
+            snake.Grow();
+            GameManager.Instance.AddScore(100);
+            SpawnFood(snake.GetOccupiedCells());
+        }
+    }
+
+    void SpawnFood(List<Vector2Int> occupied)
+    {
+        Vector2Int pos;
+        int attempts = 0;
+
+        do {
+            pos = GridManager.Instance.GetRandomCell();
+            attempts++;
+        } while (occupied != null && occupied.Contains(pos) && attempts < 200);
+
+        foodGridPos = pos;
+        transform.position = GridManager.Instance.GridToWorld(pos);
+    }
+}
